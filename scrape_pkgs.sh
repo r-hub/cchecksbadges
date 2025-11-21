a98655818fedef7dc58cc996c75d5061970c5113
Scott Chamberlain <sckott@fastmail.com>
2025-11-20 20:44:10 -0800

    -o to --output-directory

--- scrape_pkgs.sh
+++ scrape_pkgs.sh
@@ -3,5 +3,5 @@ url='https://crandb.r-pkg.org/-/pkgnames'
 url_pat='https://cran.rstudio.com/web/checks/check_results_%s.html\n'
 curl $url > names.json
 cat names.json | jq '. | keys[]' > names.txt
-cat names.txt | xargs printf $url_pat | ganda --connect-timeout-millis 10000 --workers 40 -o /tmp/htmls
+cat names.txt | xargs printf $url_pat | ganda --connect-timeout-millis 10000 --workers 40 --output-directory /tmp/htmls
 echo "done"
